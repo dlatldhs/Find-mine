1f5aec72991bcb6c4057cddc9379cb4ed40a4ed2
dlatldhs <sionzz1713@gmail.com>
2021-11-02 20:10:23 +0900

    전체적인 함수 구현 완료

--- main.py
+++ main.py
@@ -61,8 +61,90 @@ for _ in range(MINE_COUNT):
 
 clock = pygame.time.Clock() 
 
+# 마우스 포인터를 이용해서 마우스에 접근 함==============================
+# 마우스가 셀 안에 있는 건지 확인을 해주는 함수
 def in_bound(column_index, row_index):
     if (0 <= column_index < COLUMN_COUNT and 0 <= row_index < ROW_COUNT):
         return True
     else:
         return False
+#========================================================================
+
+
+def open_tile(column_index, row_index): 
+    if not in_bound(column_index, row_index):# 셀 안에 있는 거면 1이 리턴되는데 그게 not 반대니까 0 이라서 실행이 안됨
+        return
+
+    tile = grid[row_index][column_index]
+    #셀이 열려있지 않으면 활성화 True
+    if not tile['open']:
+        tile['open'] = True
+    #아니면 리턴
+    else:    
+        return
+
+    #지뢰면 리턴 
+    if tile['mine']:
+        return
+
+    #주변의 지뢰 개수를 리턴 함    
+    #밑에 함수 실행 후 주변의 ☆비어 있는 셀☆ 들을 전부 오픈 함
+    mine_count_around = get_mine_count_around(column_index, row_index)
+    if mine_count_around > 0:
+        tile['mine_count_around'] = mine_count_around
+    else:
+        for dc, dr in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]:
+            column_index_around, row_index_around = (column_index + dc, row_index + dr)
+            open_tile(column_index_around, row_index_around)
+
+# 그 블록을 중심으로 9칸을 다 둘러보고 지뢰가 몇 개 있는지 계산해서 결과를 리턴해서 셀에다가 나타냄
+def get_mine_count_around(column_index, row_index):
+    count = 0
+
+    for dc, dr in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]:
+        column_index_around, row_index_around = (column_index + dc, row_index + dr)
+        if in_bound(column_index_around, row_index_around) and grid[row_index_around][column_index_around]['mine']:
+            count += 1
+    return count
+#===============================================사용할-함수들-끝======================================================
+
+def runGame():
+    SUCCESS = 1
+    FAILURE = 2
+    game_over = 0
+ 
+    while True: 
+        clock.tick(30) 
+        screen.fill(BLACK) 
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                break
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                column_index = event.pos[0] // CELL_SIZE
+                row_index = event.pos[1] // CELL_SIZE
+                if event.button == 1:
+                    if in_bound(column_index, row_index):
+                        tile = grid[row_index][column_index]
+                        if tile['mine']:
+                            tile['open'] = True
+                            game_over = FAILURE
+                        else:
+                            open_tile(column_index, row_index)
+                elif event.button == 3:
+                    if in_bound(column_index, row_index):
+                        tile = grid[row_index][column_index]
+                        if not tile['flag']:
+                            tile['flag'] = True
+                        else:
+                            tile['flag'] = False
+ 
+                        success = True
+                        for row_index in range(ROW_COUNT):
+                            for column_index in range(COLUMN_COUNT):
+                                tile = grid[row_index][column_index]
+                                if tile['mine'] and not tile['flag']:
+                                    success = False
+                                    break
+                        if success:
+                            game_over = SUCCESS
