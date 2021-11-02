674b142846cd2a1f65748675122c4963a43ba19d
dlatldhs <sionzz1713@gmail.com>
2021-11-02 19:31:57 +0900

    pygame 지뢰 제작 셀 제작

--- main.py
+++ main.py
@@ -19,3 +19,27 @@ GRAY = (128, 128, 128)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 #=======색깔=============
+#=======게임-폰트-설정=====================
+large_font = pygame.font.SysFont(None, 72)
+small_font = pygame.font.SysFont(None, 36)
+#=======게임-폰트-설정=====================
+
+#===========display-size==================
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+#===========display-size==================
+
+#CELL_SIZE 가로 셀 과 세로 셀 에 들어갈 지뢰의 개수
+CELL_SIZE = 50
+COLUMN_COUNT = SCREEN_WIDTH // CELL_SIZE
+ROW_COUNT = SCREEN_HEIGHT // CELL_SIZE
+#================================================
+
+#=============================================================GRID 라는 리스트를 만들어서==============================================
+# mine = 지뢰의 여부 
+# open은 활성화 여부
+# mine_count_around는 주변의 지뢰 개수 
+# flag 는 flag활성화 여부 
+grid = [[{'mine': False, 'open': False, 'mine_count_around': 0, 'flag': False} for _ in range(COLUMN_COUNT)] for _ in range(ROW_COUNT)]
+#======================================================================================================================================
